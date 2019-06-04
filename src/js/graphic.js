f1be6b80671128890a764ebb4042775858abf547
russellgoldenberg <russellgoldenberg@gmail.com>
2019-06-04 10:35:23 -0400

    brighton hove

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -142,7 +142,7 @@ function setupMap() {
 
   map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/dock4242/cjwfdby852jaf1cqeqpdqa9i8',
+    style: 'mapbox://styles/dock4242/cjwhwmgzi0kdc1cqiqb86brqr',
     center: [-3.436, 55.3781],
     zoom: 4,
     maxBounds: [[-20, 45], [15, 65]],
